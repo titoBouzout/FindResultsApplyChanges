591d56ec57c3a02d5260986e73bcd1ffa264a370
Tito Bouzout <tito.bouzout@gmail.com>
2014-02-04 02:40:24 -0200

    Wataá! huu!

---
+++ FindResultsCommitChanges.py
@@ -0,0 +1,76 @@
+# coding=utf8
+import sublime, sublime_plugin, re
+
+class FindResultsCommitChanges(sublime_plugin.WindowCommand):
+	def run(self):
+		for view in sublime.active_window().views():
+			if view.name() == 'Find Results':
+
+			# get "Find Results" buffer contents
+
+				_content = view.substr(sublime.Region(0, view.size())).split("\n")
+				# pop the header "Searching 2 files for "aa" and the empty line
+				_content.pop(0)
+				_content.pop(0)
+
+				# get an iter
+				_content = iter(_content)
+
+				# regexps
+				_match_numbered = re.compile(r'^\s+([0-9]+|\.+$)', re.I);
+				_match_changed = re.compile(r'^\s+[0-9]+\:', re.I);
+
+				# the first file name
+				file_name = str(re.sub(r'\:$', '', next(_content)))
+
+				# the list of changes to apply
+				changes = {}
+
+			# get the changes
+
+				for line in iter(_content):
+					l = str(line)
+					#print('about file:'+file_name);
+					while _match_numbered.match(l):
+						if _match_changed.match(l):
+							#print('have to change'+l)
+							try:
+								changes[file_name]
+							except:
+								changes[file_name] = {}
+							# hold the change in the file name with line number+1 as index and content as value
+							changes[file_name][re.sub(r"^\s+([0-9]+)\:.*", "\\1", l)]  = re.sub(r"^\s+[0-9]+\: (.*)", '\\1', l)
+						line = next(_content);
+						l = str(line)
+					file_name = re.sub(r'\:$', '', l)
+
+				# print(changes);
+
+			# apply changes
+
+				for f in changes:
+					f = f.strip();
+					if f:
+						print('about modifying file '+f)
+						content = self.read(f).split('\n');
+						for k in changes[f].keys():
+							# print('Line number: '+k+'-1')
+							# print('Has new value: '+changes[f][k]);
+							content[int(k)-1] = changes[f][k]
+						self.write(f, "\n".join(content))
+
+
+	def is_enabled(self):
+		return self.view_exists()
+
+	def view_exists(self):
+		for view in sublime.active_window().views():
+			if view.name() == 'Find Results':
+				return True
+		return False
+
+	def read(self, name):
+		return open(name, 'r', newline='').read()
+
+	def write(self, name, content):
+		open(name, 'w+', encoding='utf8', newline='').write(str(content))
---
+++ readme.md
@@ -0,0 +1,32 @@
+***[Sublime Text 3+](http://www.sublimetext.com/) Package. Install via an updated version of  [Package Control 2+](https://sublime.wbond.net/installation). Just **DON'T** install manually.
+
+# Find Results Commit Changes
+
+## Description
+
+**Not ready for primetime.** Experimental package with the purpose to "commit" any change you made to a "Find Results" buffer. ie:
+- Search for "a" in a folder.
+- This will open a "Find Results" buffer listing all the files with "a" in it.
+- Change the "a" for a "b" in these lines that match content.
+- Go to -> "Main menubar" -> "Find" -> "Find Results Commit Any Change Made"
+- This will write all the changes made back to all the files.
+
+## Bugs
+
+- Will only modify these lines starting with a number and a colon. ie "3425:" **only**
+- Double click in these lines with numbers and a colon will open the file :-/  ST default behaviour(how to disable it?)
+- Will not modify any other line, even if you insert a new line.
+- WONTFIX: Will write/read UTF8 files, if you have a file in another encoding, considering jumping to the U8 world. :)
+- Will open read and write the files with the new lines, even if there is no modifications.
+
+## Source-code
+
+https://github.com/SublimeText/FindResultsCommitChanges
+
+## Forum Thread
+
+http://www.sublimetext.com/forum/viewtopic.php?f=6&t=14118
+
+# Like it? Support
+
+- https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=YNNRSS2UJ8P88&lc=UY&item_name=Support%20%20SideBarEnhancements%20Developer&amount=12%2e00&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted

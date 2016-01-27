92efd9f8947545cf640f5ababf5b2a052da978df
Tito Bouzout <tito.bouzout@gmail.com>
2016-01-27 18:34:59 -0300

    Workaround folder_exclude_patterns vs file_exclude_patterns vs index_exclude_patterns vs binary_file_patterns mess, this just now marks everything as binary.

--- FindExcludePatterns.py
+++ FindExcludePatterns.py
@@ -1,5 +1,5 @@
 import sublime_plugin, sublime
-import fnmatch
+import re
 
 class FindExcludePatternsOMG(sublime_plugin.EventListener):
 
@@ -7,19 +7,32 @@ class FindExcludePatternsOMG(sublime_plugin.EventListener):
         if command_name == 'show_panel' and 'panel' in args and args['panel'] == 'find_in_files' and 'FindExcludePatternsOMG' not in args:
             s = sublime.load_settings('Preferences.sublime-settings')
             exclude = list(set(list(s.get('index_exclude_patterns', []) +
-                                    s.get('binary_file_patterns', []) +
-                                    s.get('find_exclude_patterns', []))))
+                                    s.get('binary_file_patterns', []))))
 
-            if 'where' in args:
-                # if the WHERE argument is present then remove from the exlusion list any of these item that matches WHERE we want to find.
+            for k, v in enumerate(exclude):
+                exclude[k] = exclude[k].replace('\\', '/')
+            exclude = sorted(exclude)
+
+            if 'where' in args and args['where']:
+                where = args['where']
+                where = where.replace('*', '')
+                where = re.sub('([a-z])\:/', '', where)
+                where = re.sub('/$', '', where)
                 new_exclude = []
                 for item in exclude:
-                    if not args['where'] or (not fnmatch.fnmatch(args['where'].replace('\\', '/').replace('//', '/'), item) and not fnmatch.fnmatch(args['where'].replace('\\', '/').replace('//', '/')+'/', item)):
+                    thingy = item
+                    thingy = thingy.replace('-*', '')
+                    thingy = thingy.replace('*', '')
+                    thingy = re.sub('([a-z])\:/', '', thingy)
+                    thingy = re.sub('-/([a-z])\/', '', thingy)
+                    thingy = re.sub('/$', '', thingy)
+                    if thingy not in where:
                         new_exclude.append(item)
                 args['where'] = args['where'] + ',' + ('-'+(',-'.join(new_exclude)))
             else:
                 args['where'] = '-'+(',-'.join(exclude))
 
+            args['where'] = re.sub('([a-z])\:/', '/\\1/', args['where'].replace('\\', '/'), 0, re.I)
             args['FindExcludePatternsOMG'] = 1
 
             return (command_name, args)
